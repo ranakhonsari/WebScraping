a8492a86054c62cea9f50d8f1d8445ee64d209ef
sepehrb98 <sepehrbaharloo@gmail.com>
2020-04-20 12:12:17 +0430

    code changed to get number of rooms.

--- Final.py
+++ Final.py
@@ -5,15 +5,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-
 def GetData():
     price = driver.find_elements(By.XPATH, "//div[@class='sell-value']")
     size = driver.find_elements(By.XPATH, "/html/body/div/div/div/div[1]/div[3]/div[2]/div[3]/div/a/div/div[2]/div/div[2]/div[1]/div/div/span[1]")
-        
-    for m, p in zip(size, price):
+    room = driver.find_elements(By.XPATH, "/html/body/div/div/div/div[1]/div[3]/div[2]/div[3]/div/a/div/div[2]/div/div[2]/div[3]/div/div/span[1]/span")
+       
+    for p, s, r in zip(price, size, room):
         gheymat = price_to_float(p.text)
-        metraj = int(m.text)
-        cursor.execute(save_data, (metraj, gheymat))
+        metraj = int(s.text)
+        r = int(r.text)
+        cursor.execute(save_data, (metraj, r, gheymat))
         cnx.commit()
 
 
@@ -22,7 +23,7 @@ def wait_for_page_load(browser, timeout=100):
     old_page = browser.find_element_by_xpath("//*[@id='result-row']/div[1]/a/div/div[2]/span")
     yield
     WebDriverWait(browser, timeout).until(EC.staleness_of(old_page))
-    
+
 
 def price_to_float(price):
     price_rejex = '([0-9]+)'
@@ -47,8 +48,8 @@ driver.implicitly_wait(100)
 cnx = mysql.connector.connect(user='root', password='', host='127.0.0.1', database='learn')
 
 cursor = cnx.cursor()
-create_table = 'CREATE TABLE houses (metraj INT(3), price VARCHAR(20));'
-save_data = 'INSERT INTO houses (metraj, price) VALUES (%s, %s)'
+create_table = 'CREATE TABLE houses (metraj INT(3), rooms INT(1), price VARCHAR(20));'
+save_data = 'INSERT INTO houses (metraj, rooms, price) VALUES (%s, %s, %s)'
 cursor.execute(create_table)
 print("Saving data...")
 for i in range(10):
